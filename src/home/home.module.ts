76624b49b78de61bce2a7cf4ab3dce0657703872
afmercer <mercer20@up.edu>
2019-02-11 20:35:03 -0800

    Map updates on load data
    Map updates its view to show a zoomed in view on the specific region that the user selected

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,13 +1,13 @@
 import {BrowserModule, Title} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
-
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import {HomeComponent} from './home.component';
 import {MaterialModule} from "../helpers/material.module";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, LeafletModule],
   exports: [HomeModule],
   providers: [Title]
 })
