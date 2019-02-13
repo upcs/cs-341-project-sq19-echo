cf72fca06ef13b4c755d1c07145c938a9a45e950
Alex Hadi <alexhisb@gmail.com>
2019-02-12 16:44:10 -0800

    Some changes to Leaflet flyTo calls.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {Title} from "@angular/platform-browser";
-import * as $ from 'jquery';
-
-interface NavbarLabel {
-  readonly path: string;
-  readonly title: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,14 +7,9 @@ interface NavbarLabel {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  navLabels: NavbarLabel[] = [
-    {path: 'home', title: 'Home'},
-    {path: 'about', title: 'About'},
-    {path: 'login', title: 'Login'}
-  ];
+  navLabels: string[] = ['home', 'about', 'login'];
 
   @ViewChild('navItemList') navItemList: ElementRef;
-  @ViewChild('test') test: ElementRef;
 
   public constructor(private titleService: Title) {
     titleService.setTitle("Echo App");
