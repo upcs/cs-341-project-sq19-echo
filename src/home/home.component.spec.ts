e25cacb576ba4cb2fbe75ecf7030b3888f1d6d2e
Alex Hadi <alexhisb@gmail.com>
2019-02-14 08:16:39 -0800

    Replaced dummy tests and added other tests. Moved all images to assets folder.

--- src/home/home.component.spec.ts
+++ src/home/home.component.spec.ts
@@ -1,8 +1,29 @@
 import {TestBed, async} from '@angular/core/testing';
 import {HomeComponent} from './home.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {MaterialModule} from "../helpers/material.module";
+import {LeafletModule} from "@asymmetrik/ngx-leaflet";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('Home Tests', () => {
-  test('2 + 2 should be 4', () => {
-    expect(2 + 2).toBe(4);
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MaterialModule, LeafletModule, BrowserAnimationsModule],
+      declarations: [HomeComponent],
+    }).compileComponents();
+  }));
+
+  test('should create home component properly', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const homeComponent = fixture.debugElement.componentInstance;
+    expect(homeComponent).toBeTruthy();
+  });
+
+  test('should have proper heading', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('Portland Traffic Reform');
   });
 });
--- src/login/login.component.spec.ts
+++ src/login/login.component.spec.ts
@@ -1,8 +1,26 @@
 import {TestBed, async} from '@angular/core/testing';
 import {LoginComponent} from './login.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('Login Tests', () => {
-  test('2 + 2 should be 4', () => {
-    expect(2 + 2).toBe(4);
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [LoginComponent],
+    }).compileComponents();
+  }));
+
+  test('should create login component properly', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const loginComponent = fixture.debugElement.componentInstance;
+    expect(loginComponent).toBeTruthy();
+  });
+
+  test('should have proper heading', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent)
+      .toContain('Please Login or Create an account');
   });
 });
