46f3cf460d67ceef33a1c7892ebe8733adda671b
Alex Hadi <alexhisb@gmail.com>
2019-02-24 22:09:47 -0800

    Fixed the failing tests (CookieService needed to be mocked).

---
+++ __mocks__/ngx-cookie-service.js
@@ -0,0 +1,36 @@
+// Citation: https://stackoverflow.com/questions/51731016/unexpected-token-export-jest-angular
+class MockCookieService {
+  constructor() {
+    this._cookieReg = {};
+  }
+  check(name) {
+    return !!this._cookieReg[name];
+  }
+  get(name) {
+    return this._cookieReg[name];
+  }
+  getAll() {
+    return this._cookieReg;
+  }
+  set(
+    name,
+    value,
+    expires,
+    path,
+    domain,
+    secure,
+    sameSite
+  ) {
+    this._cookieReg[name] = name + '=' + value;
+  }
+  delete(name, path, domain) {
+    delete this._cookieReg[name];
+  }
+  deleteAll(path, domain) {
+    this._cookieReg = {};
+  }
+}
+
+module.exports = {
+  CookieService: MockCookieService
+};
