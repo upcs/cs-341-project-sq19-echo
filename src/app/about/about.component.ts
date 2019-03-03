04d72ecc3998295215a7248f966a1f02e6969cfe
Alex Hadi <alexhisb@gmail.com>
2019-03-03 09:29:00 -0800

    Changed the about page to use *ngFor directive and (click) instead of onclick.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,57 @@
 import {Component} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {PersonInfo} from "./about.interfaces";
 
 @Component({
   selector: 'app-root',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-
 export class AboutComponent {
+  people: PersonInfo[] = [
+    {
+      name: 'Alex Hadi',
+      classStanding: 'Junior',
+      imageUrl: 'https://tinyurl.com/y25pe2wy',
+      mainContent: `Alex Hadi wants to teach you about angular.
+                    He really enjoys doing all of the sprints, and wants to become a usain bolt type sprinter.`,
+      emailAddress: 'hadi20@up.edu'
+    },
+    {
+      name: 'Adam Mercer',
+      classStanding: 'Junior',
+      imageUrl: 'https://tinyurl.com/y4tlsscn',
+      mainContent: 'Is really good at finding problems... not solving them.',
+      emailAddress: 'mercer20@up.edu'
+    },
+    {
+      name: 'Pele Kamala',
+      classStanding: 'Sophomore',
+      imageUrl: 'https://tinyurl.com/y2xure62',
+      mainContent: 'Is very eager to work on SQL with Alex.',
+      emailAddress: 'kamala21@up.edu'
+    },
+    {
+      name: 'Callum Morham',
+      classStanding: 'Junior',
+      imageUrl: 'https://tinyurl.com/y2k4q5uq',
+      mainContent: 'Is the embodiment of larry the lobster.',
+      emailAddress: 'morham20@up.edu'
+    },
+    {
+      name: 'Chris Sebrechts',
+      classStanding: 'Junior',
+      imageUrl: 'https://tinyurl.com/y4zuw9pr',
+      mainContent: 'Does not like algorithms.',
+      emailAddress: 'sebrecht20@up.edu'
+    }
+  ];
+
   public constructor(private titleService: Title) {
     titleService.setTitle('About Page');
+  }
+
+  openEmail(email: string): void {
+    window.open(`mailto:${email}`, "_parent");
   }
 }
---
+++ src/app/about/about.interfaces.ts
@@ -0,0 +1,7 @@
+export interface PersonInfo {
+  name: string;
+  classStanding: string;
+  imageUrl: string;
+  mainContent: string;
+  emailAddress: string;
+}
