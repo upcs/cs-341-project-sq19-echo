2df0718edfae7696939bef8e885379613181da6e
Alex Hadi <alexhisb@gmail.com>
2019-03-17 15:30:51 -0700

    Updates to homepage function access levels and names.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -27,10 +27,6 @@ import {DENSITIES} from './home.component.constants';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  // For template to function properly.
-  objectKeys = Object.keys;
-  densities = DENSITIES;
-
   private DEFAULT_COORDS: LatLngExpression = [45.5122, -122.6587];
   private DEFAULT_INTENSITY_RANGE: DensityInfo = {min: 0, max: 100000};
 
@@ -38,10 +34,12 @@ export class HomeComponent {
   private leafletMarkers: Marker[] = [];
   private map: LeafletMap;
 
-  years: string[] = ['2019', '2018', '2017', '2016', '2015', '2014'];
-  vehicles: string[] = Object.keys(VehicleType);
-
-  areas: {[location: string]: LatLngExpression} = {
+  // Fields accessed by the HTML (template).
+  public objectKeys = Object.keys;
+  public densities = DENSITIES;
+  public years: string[] = ['2019', '2018', '2017', '2016', '2015', '2014'];
+  public vehicles: string[] = Object.keys(VehicleType);
+  public areas: {[location: string]: LatLngExpression} = {
     [TrafficLocation.North]: [45.6075, -122.7236],
     [TrafficLocation.South]: [45.4886, -122.6755],
     [TrafficLocation.Northwest]: [45.5586, -122.7609],
@@ -50,7 +48,8 @@ export class HomeComponent {
     [TrafficLocation.Southeast]: [45.4914, -122.5930]
   };
 
-  leafletOptions: MapOptions = {
+  // Used by the HTML/template to set Leaflet's options.
+  public leafletOptions: MapOptions = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
@@ -71,7 +70,7 @@ export class HomeComponent {
     titleService.setTitle('Portland Traffic Reform');
   }
 
-  isRelevantMarker(trafficMarker: TrafficMarker): boolean {
+  private isRelevantMarker(trafficMarker: TrafficMarker): boolean {
     const selectedYear = this.yearSelector.empty ? '-' : this.yearSelector.value;
     const selectedDensity = this.densitySelector.empty ? this.DEFAULT_INTENSITY_RANGE : this.densitySelector.value;
     const selectedVehicleFilter = getVehicleFilterFromVehicleSelectorValue(this.vehicleSelector.value);
@@ -81,7 +80,13 @@ export class HomeComponent {
         && markerValidForVehicleFilter(trafficMarker, selectedVehicleFilter);
   }
 
-  updateDisplayedTrafficMarkers(): void {
+  private updateLeafletMapLocation(): void {
+    const coordinates = this.areaSelector.empty ? this.DEFAULT_COORDS : this.areaSelector.value.coordinates;
+    const zoom = this.areaSelector.empty ? 11 : 12.5;
+    this.map.flyTo(coordinates, zoom);
+  }
+
+  private updateDisplayedLeafletMarkers(): void {
     this.leafletMarkers.forEach(marker => this.map.removeLayer(marker));
 
     const relevantTrafficMarkers: TrafficMarker[] = this.allTrafficMarkers.filter(
@@ -94,32 +99,33 @@ export class HomeComponent {
         return leafletMarker;
       }
     );
-
-    const coordinates = this.areaSelector.empty ? this.DEFAULT_COORDS : this.areaSelector.value.coordinates;
-    const zoom = this.areaSelector.empty ? 11 : 12.5;
-    this.map.flyTo(coordinates, zoom);
   }
 
-  clearFilters(): void {
+  public updateMap(): void {
+    this.updateDisplayedLeafletMarkers();
+    this.updateLeafletMapLocation();
+  }
+
+  public clearFiltersAndUpdateMap(): void {
     this.areaSelector.value = '';
     this.yearSelector.value = '';
     this.vehicleSelector.value = '';
     this.densitySelector.value = '';
 
-    this.updateDisplayedTrafficMarkers();
+    this.updateMap();
   }
 
   /**
    * Initialize Leaflet map.
    * @param map The Leaflet map to initialize.
    */
-  onMapReady(map: LeafletMap): void {
+  public onMapReady(map: LeafletMap): void {
     this.map = map;
 
     const TRAFFIC_URL = 'https://opendata.arcgis.com/datasets/6ba5258ffea34e878168ddc8cf34f7e3_250.geojson';
     this.http.get(TRAFFIC_URL).subscribe((trafficJson: FeatureCollection) => {
       this.allTrafficMarkers = getTrafficMarkersFromFeatures(trafficJson.features);
-      this.updateDisplayedTrafficMarkers();
+      this.updateMap();
     });
   }
 }
