8de2d4da3241d88bc9bf4ac4925685360570e9a7
Alex Hadi <alexhisb@gmail.com>
2019-03-31 13:54:48 -0700

    Initial port to Express. Still some issues.

--- src/app/components/about/about.component.interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface PersonInfo {
-  readonly name: string;
-  readonly classStanding: string;
-  readonly imageUrl: string;
-  readonly mainContent: string;
-  readonly emailAddress: string;
-}
